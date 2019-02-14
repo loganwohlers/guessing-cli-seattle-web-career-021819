dbb51cc34b82229c172253507c150dfc2070ccd4
Logan Wohlers <lawohlers@gmail.com>
2019-02-14 01:56:33 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   answer=gets.chomp
   unless answer=='exit'
-    if answer.to_i==
+    #if answer.to_i==
   end
   
   puts "Goodbye!"
